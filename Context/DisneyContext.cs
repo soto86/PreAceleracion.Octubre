d796cd7bc6b6852710f7800dfc27fc61862aa386
santiagosoto <santiago_Soto86@yahoo.com.ar>
2021-10-23 20:58:05 -0300

    Agregar archivos de proyecto.

---
+++ Context/DisneyContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PreAceleracion.Octubre.Entities;
+
+namespace PreAceleracion.Octubre.Context
+{
+    public class DisneyContext : DbContext
+    {
+        private const string Schema = "disney";
+        public DisneyContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema(Schema);
+            modelBuilder.Entity<Character>().HasData(
+                new Character()
+                {
+                    Id = 1,
+                    Image = null
+                });
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Entities/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreAceleracion.Octubre.Entities
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+
+    }
+}
